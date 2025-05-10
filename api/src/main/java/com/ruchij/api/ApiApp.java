518cf35c9826bb0848d26fe7041fd0079bb062d2
Ruchira (Richie) Jayasekara <me@ruchij.com>
2025-05-10 20:23:35 +1000

    Add localhost:3000 to CORS allowed hosts
    Included "http://localhost:3000" in the CORS configuration to support local development on an additional port. This ensures proper cross-origin requests for environments running on localhost:3000.

--- api/src/main/java/com/ruchij/api/ApiApp.java
+++ api/src/main/java/com/ruchij/api/ApiApp.java
@@ -80,7 +80,7 @@ public class ApiApp {
 
             javalinConfig.bundledPlugins.enableCors(cors -> {
                 cors.addRule(rule -> {
-                    rule.allowHost("http://localhost:5173", "*.ruchij.com");
+                    rule.allowHost("http://localhost:5173", "http://localhost:3000", "*.ruchij.com");
                     rule.allowCredentials = true;
                 });
             });
